28015317665d597cff528dc759bf3ce9e3db6b13
Julian Andres Guzman Cuellar <julianguzmn28@gmail.com>
2024-04-19 12:48:01 -0500

    Añadimos comentarios

--- POO/src/SolicitarRadioCirculo/AreaCirculo.java
+++ POO/src/SolicitarRadioCirculo/AreaCirculo.java
@@ -12,11 +12,15 @@ public class AreaCirculo {
     double PI = 3.14159265359;
 
     public void pedirNumero() {
+        //Uso una interfaz gráfica para pedir el número 1 con el cual se hallará el radio (Quiero aprender a usar eso)
         numero1 = Integer.parseInt(JOptionPane.showInputDialog("inserte un número"));
     }
 
     public void calcularRadio(int radio) {
+        
+        //Usando la formula para hallar el radio, hallamos el radio
         double area = 2 * PI * radio * radio;
+        //Mostramos el resultado
         JOptionPane.showMessageDialog(null, "El área del círculo es: " + area);
 
     }
--- POO/src/SolicitarRadioCirculo/main.java
+++ POO/src/SolicitarRadioCirculo/main.java
@@ -3,6 +3,7 @@ package SolicitarRadioCirculo;
 public class main {
     
     public static void main(String[] args) {
+
         AreaCirculo ac = new AreaCirculo();
         ac.pedirNumero();
         ac.calcularRadio(0);
