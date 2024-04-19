89c1130d4d28cab8e171812dc23ba798bac2bca3
Julian Andres Guzman Cuellar <julianguzmn28@gmail.com>
2024-04-19 12:27:58 -0500

    EmpezamoEmpezamoss

--- POO/src/SolicitarRadioCirculo/AreaCirculo.java
+++ POO/src/SolicitarRadioCirculo/AreaCirculo.java
@@ -1,9 +1,21 @@
-package SolicitarRadioCirculo;
 /**
  *
  * @author JulianAndresGuzmanCuellar 20232215518
  */
+package SolicitarRadioCirculo;
+import javax.swing.JOptionPane;
 
 public class AreaCirculo {
+    private static final double PI = 3.14159265359;
+
+    public void pedirNumero() {
+        String entrada = JOptionPane.showInputDialog(null, "Introduce el radio del círculo:");
+    }
+    int numero1 = Integer.parseInt(entrada);
+    
+    public void calcularRadio(){
+        double area = 2 * PI * numero1 * numero1;
+    
+    }
     
 }
