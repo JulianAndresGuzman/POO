d090a9619ebe07461990e5e35c9f0b40d04db34c
Julian Andres Guzman Cuellar <julianguzmn28@gmail.com>
2024-04-19 22:23:28 -0500

    Añadimos comentarios

--- POO/src/SieteProductoIva/EjecutorCodigo.java
+++ POO/src/SieteProductoIva/EjecutorCodigo.java
@@ -8,7 +8,9 @@ public class EjecutorCodigo {
     
     public static void main(String[] args) {
         
+        //Pedimos el nombre al usuario
         String nombre = JOptionPane.showInputDialog(null, "Por favor, introduce tu nombre:");
+        //Le damos bienvenida al usuario
         JOptionPane.showMessageDialog(null, "Hola " + nombre + " bienvenid@ a mi asombroso programa");
         Producto prd = new Producto();
         prd.pedirPrecio();
--- POO/src/SieteProductoIva/Producto.java
+++ POO/src/SieteProductoIva/Producto.java
@@ -6,18 +6,22 @@ import javax.swing.JOptionPane;
  */
 public class Producto {
 
+    //Introducimos las variables
     int producto;
     public static final double iva = 0.21;
     double resultadoFinal;
 
+    //Pedimos el precio con interfaz
     public void pedirPrecio() {
         producto = Integer.parseInt(JOptionPane.showInputDialog("inserte un precio"));
     }
-
+    
+    //Calculamos el resultado
     public void definirResultado() {
         resultadoFinal = producto + (producto * iva);
     }
 
+    //Damos el resultado
     public void darResultado() {
         JOptionPane.showMessageDialog(null, "El precio final es: " + resultadoFinal + "$");
     }
