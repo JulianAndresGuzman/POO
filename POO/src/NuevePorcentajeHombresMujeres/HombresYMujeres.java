52dbf76bbd7a341be4d7003e8d8edc72ce1a1e6a
Julian Andres Guzman Cuellar <julianguzmn28@gmail.com>
2024-04-19 22:58:26 -0500

    Añadimos comentarios

--- POO/src/NuevePorcentajeHombresMujeres/HombresYMujeres.java
+++ POO/src/NuevePorcentajeHombresMujeres/HombresYMujeres.java
@@ -5,23 +5,29 @@
 package NuevePorcentajeHombresMujeres;
 import javax.swing.JOptionPane;
 public class HombresYMujeres {
+    //Importamos las variables
     int hombres, mujeres, totalPersonas;
     double porcentajeHombres, porcentajeMujeres;
  
+    //Pedimos los datos de la cantidad de hombres y mujeres
     public void pedirDatos(){
         hombres = Integer.parseInt(JOptionPane.showInputDialog("inserte cuantos hombres hay"));
         mujeres = Integer.parseInt(JOptionPane.showInputDialog("inserte cuantas mujeres hay"));
     }
     
+    //Formula para hallar los porcentajes
     public void analizarDatos(){
         totalPersonas = hombres + mujeres;
         porcentajeHombres = (hombres * 100)/ totalPersonas;
         porcentajeMujeres = (mujeres * 100)/ totalPersonas;
     }
+    //Vamos a mostrar los datos
     public void mostrarDatos(){
         JOptionPane.showMessageDialog(null, "Porcentaje de hombres:" + porcentajeHombres + "%");
         JOptionPane.showMessageDialog(null, "Porcentaje de Mujeres:" + porcentajeMujeres + "%");
     }
+    
+    //Clase main, donde ejecutaremos el código
     public static void main(String[] args) {
         HombresYMujeres porcent = new HombresYMujeres();
         porcent.pedirDatos();
