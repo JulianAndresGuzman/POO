61ea5847e4534d1bed7eb74d59f2b14185bd58bf
Julian Andres Guzman Cuellar <julianguzmn28@gmail.com>
2024-04-20 22:55:40 -0500

    Creo que ya funciona

--- POO/src/POONivelInicial/Prueba.java
+++ POO/src/POONivelInicial/Prueba.java
@@ -12,13 +12,14 @@ public class Prueba {
         int cantidadTriangulos;
         cantidadTriangulos = Integer.parseInt(JOptionPane.showInputDialog("inserte la cantidad de triangulos"));
         Triangulo[] triangulos = new Triangulo[cantidadTriangulos];
-        
-        for (int i = 0; i < cantidadTriangulos; i++ ) {
-            int base = Integer.parseInt(JOptionPane.showInputDialog("inserte la base del triángulo " + (i + 1)));
-            triangulos[i] = new Triangulo(pedirDatos);
 
+        for (int i = 0; i < cantidadTriangulos; i++) {
+            int base = Integer.parseInt(JOptionPane.showInputDialog("inserte la base del triángulo " + (i + 1)));
+            int altura = Integer.parseInt(JOptionPane.showInputDialog("inserte la altura del triángulo " + (i + 1)));
+            triangulos[i] = new Triangulo(base, altura);
+            triangulos[i].calcularArea();
             
-            
+            JOptionPane.showMessageDialog(null, "El área del triángulo " + (i + 1) + " es: " + triangulos[i].getArea());
         }
     }
 }
