c1332d954eba18eefed4373bdfbf837b41e3e2f0
Julian Andres Guzman Cuellar <julianguzmn28@gmail.com>
2024-04-24 13:33:03 -0500

    Todo error...

--- POO/src/sieteb15/GrupoPersonas.java
+++ POO/src/sieteb15/GrupoPersonas.java
@@ -6,7 +6,21 @@ package sieteb15;
  * @author Usuario
  */
 public class GrupoPersonas {
-     private Persona[] personas; // Arreglo para almacenar las personas
+    private Persona[] personas; // Arreglo para almacenar las personas
     private int cantidadPersonas; // Contador para llevar la cantidad de personas en el grupo
     
+    public GrupoPersonas() {
+        this.personas = new Persona[100];
+        this.cantidadPersonas = 0;
+        
+    }
+    public void agregarPersona(Persona persona) {
+        // Para Verificar si el grupo no está lleno
+        if (cantidadPersonas < 100) {
+            personas[cantidadPersonas] = persona;
+            cantidadPersonas++;
+        } else {
+            System.out.println("El grupo ya está completo");
+        }
+    }
 }
