98afeb11edf531b8dabe50aec1881ea29e67f260
Julian Andres Guzman Cuellar <julianguzmn28@gmail.com>
2024-04-24 13:31:37 -0500

    aa

--- POO/src/sieteb15/GrupoPersonas.java
+++ POO/src/sieteb15/GrupoPersonas.java
@@ -6,5 +6,7 @@ package sieteb15;
  * @author Usuario
  */
 public class GrupoPersonas {
+     private Persona[] personas; // Arreglo para almacenar las personas
+    private int cantidadPersonas; // Contador para llevar la cantidad de personas en el grupo
     
 }
